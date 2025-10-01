d250acbbedeb68a87357076e5c69b0e1e80929a3
ganzabruce <ganzabruce10@gmail.com>
2025-10-01 10:33:36 +0200

    refined my auth for admin and embedded the auth in one file

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,8 @@
 import express from "express"
 import { signinUser } from "../controllers/auth/signin"
 import { signupUser } from "../controllers/auth/signup"
-import { signinAdmin } from "../controllers/admin/signin"
-import { signupAdmin } from "../controllers/admin/signup"
+// import { signinAdmin } from "../controllers/admin/signin"
+// import { signupAdmin } from "../controllers/admin/signup"
 
 const authRouter = express.Router()
 
@@ -128,8 +128,8 @@ const authRouter = express.Router()
 
 authRouter.post("/signup",signupUser)
 authRouter.post("/login",signinUser)
-authRouter.post("/admin/signup",signupAdmin)
-authRouter.post("/admin/login",signinAdmin)
+// authRouter.post("/admin/signup",signupAdmin)
+// authRouter.post("/admin/login",signinAdmin)
 
 
 export default authRouter
